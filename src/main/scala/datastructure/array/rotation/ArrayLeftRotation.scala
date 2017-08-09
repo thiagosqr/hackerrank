f048739cbd7f4a9f82a2b6fdcffb892c6906cabe
Thiago Siqueira <thiago.sqr@gmail.com>
2017-08-09 21:12:08 +0100

    Solving LeftRotation

--- src/main/scala/datastructure/array/rotation/ArrayLeftRotation.scala
+++ src/main/scala/datastructure/array/rotation/ArrayLeftRotation.scala
@@ -1,11 +1,26 @@
 package datastructure.array.rotation
 
+import anagrams.Solution.args
+import datastructure.stacks.balancedbrackets.Solution.{readString, sc}
+
+/**
+  *  1 2 3 4 5
+  *  left rotate twice
+  *
+  *  3 4 5 1 2
+  *
+  */
 object ArrayLeftRotation {
 
     def main(args: Array[String]): Unit = {
 
-      val arr = Array(1,2,3,4)
-      val rotation = 3
+      val sc = new java.util.Scanner (System.in);
+      var n = sc.nextInt();
+      var rotation = sc.nextInt();
+      var arr = new Array[Int](n)
+      for(a_i <- 0 to n-1) {
+        arr(a_i) = sc.nextInt();
+      }
 
       val tmp = arr.take(rotation)
 
@@ -15,7 +30,7 @@ object ArrayLeftRotation {
 
       val rotdArr = arr.take(arr.length - rotation) ++ tmp
 
-      rotdArr.foreach(System.out.print)
+      rotdArr.foreach(i => System.out.print(i + " "))
 
     }
 
